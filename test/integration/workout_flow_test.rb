bf209b71e635d9031e525ac70ecbf5b2334f6a35
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-06 15:42:31 +0800

    Test failed when redirecting after creating a post

--- test/integration/workout_flow_test.rb
+++ test/integration/workout_flow_test.rb
@@ -6,4 +6,13 @@ class WorkoutFlowTest < ActionDispatch::IntegrationTest
     get workouts_url
     assert_select "h1", "Workouts#index"
   end
+
+  test "create a workout redirects" do
+    post "/workouts",
+      params: {workout: {name: "This is my workout name"}}
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select "h2", "Workout title"
+  end
 end
