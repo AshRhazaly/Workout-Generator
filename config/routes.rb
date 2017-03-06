e1971b8b3876922a4488b5b981e2eaab6e0c50c0
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-06 14:28:09 +0800

    Add workouts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'workouts/index'
+
+  get 'workouts/new'
+
+  get 'workouts/create'
+
+  get 'workouts/show'
+
+  get 'workouts/edit'
+
+  get 'workouts/update'
+
+  get 'workouts/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
