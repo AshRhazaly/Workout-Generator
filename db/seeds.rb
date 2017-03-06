c1202905d623ead318dd159ab75f9408d58e190d
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-06 16:46:36 +0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+workouts = Workout.create([{ name: 'Jumping Jacks' }, { name: 'Normal Push Ups' }, {name: 'Crunches'},
+                          {name: 'Mountain Climbers'}, {name: 'WideGrip Push Ups'}, {name: 'Reverse Crunches'}
+                          {name: "Hip Thrusters"}, {name: "CloseGrip Push Ups"}, {name: "JackKnife"},
+                          {name: "Plank"}])
