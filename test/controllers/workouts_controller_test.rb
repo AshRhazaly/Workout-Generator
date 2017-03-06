f0c570748de5c569e5fae1f12ca13ebdfabc2f57
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-06 15:21:35 +0800

    Test passed for showing a workout

--- test/controllers/workouts_controller_test.rb
+++ test/controllers/workouts_controller_test.rb
@@ -7,6 +7,12 @@ class WorkoutsControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test "can show a workout" do
+    workout = workouts(:one)
+    get workout_url(workout)
+    assert_response :success
+  end
+
   test "can delete a workout" do
     workout = workouts(:one)
     assert_difference('Workout.count', -1) do
@@ -15,24 +21,3 @@ class WorkoutsControllerTest < ActionDispatch::IntegrationTest
   end
 
 end
-
-# test "should destroy article" do
-#   article = articles(:one)
-#   assert_difference('Article.count', -1) do
-#     delete article_url(article)
-#   end
-#
-#   assert_redirected_to articles_path
-# end
-# it "creates article" do
-#   article_params = FactoryGirl.attributes_for(:article)
-#   expect { post :create, :article => article_params }.to change(Article, :count).by(1)
-# end
-
-# test "should create article" do
-#   assert_difference('Article.count') do
-#     post articles_url, params: { article: { body: 'Rails is awesome!', title: 'Hello Rails' } }
-#   end
-#
-#   assert_redirected_to article_path(Article.last)
-# end
