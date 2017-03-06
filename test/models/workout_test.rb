d1e4da8af0d3e9b4e657d898ef75f77d70b43f63
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-06 14:23:31 +0800

    Test failed for saving empty workout

--- test/models/workout_test.rb
+++ test/models/workout_test.rb
@@ -4,4 +4,13 @@ class WorkoutTest < ActiveSupport::TestCase
   test "the workout name must be a string" do
     assert_equal true, workouts(:one).name.is_a?(String)
   end
+
+  test "the workout must not be empty" do
+    workout = Workout.new
+    assert_not workout.save
+  end
 end
+
+# posts(:one).title  = nil
+# assert_nil posts(:one).title
+# assert_equal false, posts(:one).valid?
