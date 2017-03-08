13b88032771dc87f2b41d647f3a8c98696496a91
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-08 15:02:47 +0800

    Add new seed data

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -40,6 +40,6 @@ class WorkoutsController < ApplicationController
   end
 
   def workout_params
-    params.require(:workout).permit(:name)
+    params.require(:workout).permit(:name, :repetitions)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-workouts = Workout.create([{ name: 'Jumping Jacks' }, { name: 'Normal Push Ups' }, {name: 'Crunches'},
-                          {name: 'Mountain Climbers'}, {name: 'WideGrip Push Ups'}, {name: 'Reverse Crunches'},
-                          {name: "Hip Thrusters"}, {name: "CloseGrip Push Ups"}, {name: "JackKnife"},
-                          {name: "Plank"}])
+workouts = Workout.create([{ name: 'Jumping Jacks', repetitions: 15 }, { name: 'Normal Push Ups',repetitions: 15  }, {name: 'Crunches', repetitions: 15 },
+                          {name: 'Mountain Climbers', repetitions: 15 }, {name: 'WideGrip Push Ups', repetitions: 15 }, {name: 'Reverse Crunches', repetitions: 15 },
+                          {name: "Hip Thrusters", repetitions: 15 }, {name: "CloseGrip Push Ups", repetitions: 15 }, {name: "JackKnife", repetitions: 15 },
+                          {name: "Plank", repetitions: 15 }])
