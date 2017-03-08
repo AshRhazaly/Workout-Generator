f7b8913ffc8a1f851a718e975635599294b14de1
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-08 14:26:10 +0800

    Add callback to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :generate_api_token
+
+  private
+   def generate_api_token
+     self.api_token = Devise.friendly_token
+     self.save
+   end
 end
