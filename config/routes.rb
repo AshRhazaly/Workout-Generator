bdab464c564e013747dcd4dbd01caaab13540a97
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-08 11:06:42 +0800

    Add namespace api to routes rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :workouts
+  namespace :api do
+    resources :workouts
+  end
   root "workouts#index"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
