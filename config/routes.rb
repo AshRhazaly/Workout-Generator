91fe3590aa0382dfa62194969601cc1895f6f7d9
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-08 14:15:31 +0800

    Add resources and get api token to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
   namespace :api do
     namespace :v1 do
       resources :workouts
+    end
+  end
+
+  resources :users do
+    member do
+      get 'api_token'
     end
   end
   root "workouts#index"
